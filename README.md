6da138cb6239629c1aa9b37569b39f1aaf4cbc06
Halee Mason <halee.mason@gmail.com>
2022-08-13 10:54:00 -0700

    Send half of the from wallets balance to the to wallet. Make getWalletBalance an exportable function to use in challenge2. Update README for Challenge 2. Format challenge1 file. Add gitignore.

--- README.md
+++ README.md
@@ -5,15 +5,15 @@ Create a Node project in your directory by entering:
 `npm init -y`
 
 Install the Solana Web3 JS library by entering:
-`npm install   --save @solana/web3.js`
+`npm install --save @solana/web3.js`
 
 The `index.js` file can be run with the following command:
 `node index.js`
 
 ## Challenge 1 - Keypairs and Airdrops 
-Modify the logic of the existing piece of code to get airdropped into an account of the user’s choice. To run the script, you can take the account address as a CLI parameter.
+Modify the logic of the existing piece of code to get airdropped into an account of the user's choice. To run the script, you can take the account address as a CLI parameter.
 
-The “myWallet.publicKey” should be replaced with the Public key entered by the user.
+The `myWallet.publicKey` should be replaced with the Public key entered by the user.
 
 ## Running `challenge1-solana`
 The Command line Argument `--key` is an optional argument to airdrop dev SOL into a specific wallet for testing. Example command:
@@ -22,4 +22,5 @@ The Command line Argument `--key` is an optional argument to airdrop dev SOL int
 ##  Challenge 2 - Transactions and Fees
 Explore Completed Transactions. Your challenge is to calculate the wallet balance of the sender wallet. Then, transfer 50% of the balance to another wallet.
 
-## Running `challenge1-solana`
+## Running `challenge2-solana`
+TBD
--- challenge2-solana/index.js
+++ challenge2-solana/index.js
@@ -1,78 +1,80 @@
 // Import Solana web3 functinalities
 const {
-    Connection,
-    PublicKey,
-    clusterApiUrl,
-    Keypair,
-    LAMPORTS_PER_SOL,
-    Transaction,
-    SystemProgram,
-    sendAndConfirmRawTransaction,
-    sendAndConfirmTransaction
+  Connection,
+  PublicKey,
+  clusterApiUrl,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  Transaction,
+  SystemProgram,
+  sendAndConfirmRawTransaction,
+  sendAndConfirmTransaction,
 } = require("@solana/web3.js");
 
-const DEMO_FROM_SECRET_KEY = new Uint8Array(
-    [
-        160,  20, 189, 212, 129, 188, 171, 124,  20, 179,  80,
-         27, 166,  17, 179, 198, 234,  36, 113,  87,   0,  46,
-        186, 250, 152, 137, 244,  15,  86, 127,  77,  97, 170,
-         44,  57, 126, 115, 253,  11,  60,  90,  36, 135, 177,
-        185, 231,  46, 155,  62, 164, 128, 225, 101,  79,  69,
-        101, 154,  24,  58, 214, 219, 238, 149,  86
-      ]            
-);
+const { getWalletBalance } = require("../challenge1-solana/index.js");
 
-const transferSol = async() => {
-    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+// Helper function to send half the wallet balance
+const sendHalf = (walletAmount) => {
+  console.log("walletAmount", walletAmount);
+  return Math.round(walletAmount / 2);
+};
 
-    // Get Keypair from Secret Key
-    var from = Keypair.fromSecretKey(DEMO_FROM_SECRET_KEY);
+const transferSol = async () => {
+  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
-    // Other things to try: 
-    // 1) Form array from userSecretKey
-    const from = Keypair.fromSecretKey(Uint8Array.from(userSecretKey));
-    // 2) Make a new Keypair (starts with 0 SOL)
-    const from = Keypair.generate();
+  // Get Keypair from Secret Key
+  const keypair = Keypair.generate();
+  const from = Keypair.fromSecretKey(Uint8Array.from(keypair.secretKey));
+  // Make a new Keypair (starts with 0 SOL)
+  // const from = Keypair.generate();
 
-    // Generate another Keypair (account we'll be sending to)
-    const to = Keypair.generate();
+  // Generate another Keypair (account we'll be sending to)
+  const to = Keypair.generate();
+  console.log("Receiver (to)", to.publicKey, "wallet's previous balance:");
+  await getWalletBalance(to.publicKey);
 
-    // Aidrop 2 SOL to Sender wallet
-    console.log("Airdopping some SOL to Sender wallet!");
-    const fromAirDropSignature = await connection.requestAirdrop(
-        new PublicKey(from.publicKey),
-        2 * LAMPORTS_PER_SOL
-    );
+  // Aidrop 2 SOL to Sender wallet
+  console.log("Sender (from) wallet's previous balance:");
+  let myBalance = await getWalletBalance(from.publicKey);
+  console.log("Airdopping some SOL to Sender wallet!");
+  const fromAirDropSignature = await connection.requestAirdrop(
+    new PublicKey(from.publicKey),
+    2 * LAMPORTS_PER_SOL
+  );
 
-    // Latest blockhash (unique identifer of the block) of the cluster
-    let latestBlockHash = await connection.getLatestBlockhash();
+  // Latest blockhash (unique identifer of the block) of the cluster
+  let latestBlockHash = await connection.getLatestBlockhash();
 
-    // Confirm transaction using the last valid block height (refers to its time)
-    // to check for transaction expiration
-    await connection.confirmTransaction({
-        blockhash: latestBlockHash.blockhash,
-        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-        signature: fromAirDropSignature
-    });
+  // Confirm transaction using the last valid block height (refers to its time)
+  // to check for transaction expiration
+  await connection.confirmTransaction({
+    blockhash: latestBlockHash.blockhash,
+    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    signature: fromAirDropSignature,
+  });
 
-    console.log("Airdrop completed for the Sender account");
+  console.log("Airdrop completed for the Sender account");
+  let myBalanceAfterAirdrop = await getWalletBalance(from.publicKey);
+  console.log(
+    "myBalanceAfterAirdrop",
+    myBalanceAfterAirdrop,
+    "sendHalf",
+    sendHalf(myBalanceAfterAirdrop)
+  );
+  // Send money from "from" wallet and into "to" wallet
+  var transaction = new Transaction().add(
+    SystemProgram.transfer({
+      fromPubkey: from.publicKey,
+      toPubkey: to.publicKey,
+      lamports: sendHalf(myBalanceAfterAirdrop) * LAMPORTS_PER_SOL,
+    })
+  );
 
-    // Send money from "from" wallet and into "to" wallet
-    var transaction = new Transaction().add(
-        SystemProgram.transfer({
-            fromPubkey: from.publicKey,
-            toPubkey: to.publicKey,
-            lamports: LAMPORTS_PER_SOL / 100
-        })
-    );
-
-    // Sign transaction
-    var signature = await sendAndConfirmTransaction(
-        connection,
-        transaction,
-        [from]
-    );
-    console.log('Signature is ', signature);
-}
+  // Sign transaction
+  var signature = await sendAndConfirmTransaction(connection, transaction, [
+    from,
+  ]);
+  console.log("Signature is ", signature);
+};
 
 transferSol();
